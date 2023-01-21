150265c62a0b706034c175bb1c4a47a0f37849fe
Svetoslav Aleksandrov <123257235+SvetAleksandrov@users.noreply.github.com>
2023-01-21 23:21:08 +0200

    Update

---
+++ Data Types and Variables - Lab/2poundsToDollars/Program.cs
@@ -0,0 +1,5 @@
+﻿double britishPounds = double.Parse(Console.ReadLine());
+
+decimal usdConvert = (decimal)(britishPounds * 1.31);
+
+Console.WriteLine($"{usdConvert:F3}");
---
+++ Data Types and Variables - Lab/3. Exact Sum of Real Numbers/Program.cs
@@ -0,0 +1,12 @@
+﻿int input = int.Parse(Console.ReadLine());
+decimal sum = 0;
+
+
+for (int i = 0; i < input; i++)
+
+{
+    decimal number = decimal.Parse(Console.ReadLine());
+
+    sum += number;
+}
+Console.WriteLine(sum);
